1f73d985efb16296ae18e2a98b82b9f4e3dfdfc7
AdriaPrats <apratstorrestrabajo@gmail.com>
2022-06-20 16:20:59 +0200

    Styles1

--- views/0-Connect.php
+++ views/0-Connect.php
@@ -4,28 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/0-connect.css">
     <title>Connexion</title>
 </head>
 <body>
-    <h1>Connexion a BBDD</h1>
-    <form action="connect" method="post">
-        <label for="server-ip">Ip del Servidor</label>
-        <input type="text" name="connexion_data[server-ip]" value="51.178.144.4">
+    <div class="container">
+        <h1>Connexión a BBDD</h1>
+        <form action="connect" method="post">
+        
+            <div class="group">
+                <input type="text" name="connexion_data[server-ip]" value="51.178.144.4">
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label for="server-ip">Ip del Servidor</label>
+            </div>
 
-        <label for="dbname">Nombre BBDD</label>
-        <input type="text" name="connexion_data[dbname]" value="adriaon_wp314">
+            <div class="group">
+                <input type="text" name="connexion_data[dbname]" value="adriaon_wp314">
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label for="dbname">Nombre BBDD</label>
+            </div>
 
-        <label for="user">Nombre Usuario</label>
-        <input type="text" name="connexion_data[user]" value="adriaon_wp314">
+            <div class="group">
+                <input type="text" name="connexion_data[user]" value="adriaon_wp314">
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label for="user">Nombre Usuario</label>
+            </div>
 
-        <label for="password">Contraseña</label>
-        <input type="password" name="connexion_data[password]" value="6CHjnLIQmrNz">
+            <div class="group">
+                <input type="password" name="connexion_data[password]" value="6CHjnLIQmrNz">
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label for="password">Contraseña</label>
+            </div>
 
-        <div class="buttonDiv">
-            <label for="submit">Si estan todos los valores correctos OK!</label>
-            <button type="submit" name="submit">OK!</button>
-        </div>
+            <div class="buttonDiv">
+                <label for="submit">Si estan todos los valores correctos OK!</label>
+                <button type="submit" name="submit">OK!</button>
+            </div>
 
-    </form>
+        </form>
+    </div>
 </body>
 </html>
